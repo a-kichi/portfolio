e1ff7402040c8c85a8fafc744c1acd3088753100
a-kichi <arsnino17aki24@gmail.com>
2022-03-22 02:31:16 +0900

    loading animation

---
+++ main.js
@@ -0,0 +1,43 @@
+// window.onload = function() {
+//     const spinner = document.getElementById('loading');
+//     spinner.classList.add('loaded');
+//   }
+
+
+var images = document.getElementsByTagName('img');
+var loadingBar = document.getElementById('loading_bar');
+var loadingWrap = document.getElementById('loading_barWrap');
+var loadingArea = document.getElementById('loading');
+
+var imgLen = images.length;
+var barLen = 100 / imgLen;
+console.log(barLen);
+var barWidth = 0;
+
+for (var i = 0; i < imgLen; i++) {
+    images[i].onload = function() {
+        barWidth = barWidth + barLen;
+        loadingBar.style.width = barWidth + '%';
+    }
+}
+
+function loadLen() {
+    if (!loadingArea.classList.contains('loadingNone')) {
+        if (loadingBar.clientWidth === loadingWrap.clientWidth) {
+            loadingArea.classList.add('loadingNone');
+        } else {
+            setTimeout(loadLen, 1000);
+        }
+    }
+}
+
+// 100%にならなかった時用の処理
+window.addEventListener('load', function(){
+  if (!loadingArea.classList.contains('loadingNone')) {
+    loadingBar.style.width = '100%';
+    setTimeout(function(){
+      loadingArea.classList.add('loadingNone');
+    }, 1000);
+  }
+});
+    
